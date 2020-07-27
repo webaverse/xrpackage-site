e4201f1d24d456b1845cbf7204c4f1e6185086a1
Chris <chris.eddy.dev@gmail.com>
2020-07-27 12:02:52 -0700

    remove white space

--- constants.js
+++ constants.js
@@ -1,4 +1,2 @@
-
-
 export const env = window.location.hostname === 'localhost' ||  window.location.hostname === '127.0.0.1' ? 'dev' : 'prod';
 export const apiHost = env === 'dev' ? 'http://127.0.0.1' : 'https://ipfs.exokit.org';
